10a9622619ed498c893886c62f338fef4a25a605
DEVSOG12 <oreofesolarin@gmail.com>
2022-10-27 12:27:16 -0400

    Updates

--- src/scraper.py
+++ src/scraper.py
@@ -38,7 +38,7 @@ def map_to_table(map_of_items):
 def send_simple_message(data):
     return requests.post(
         "https://api.mailgun.net/v3/sandboxc90a44973a78431e8faa47bd2b632cb2.mailgun.org/messages",
-        auth=("api", "b566292404a9b0b334646db54a1e90fe-8845d1b1-6913720c"),
+        auth=("api", "key-ded180a76d4b1efa5e19a1f50730b74b"),
         data={"from": "Research Bot <postmaster@sandboxc90a44973a78431e8faa47bd2b632cb2.mailgun.org>",
               "to": "Oreofe Solarin <khakis_forearm.0u@icloud.com>",
               "subject": "Run Complete",
